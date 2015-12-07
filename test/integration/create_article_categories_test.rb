5731d1cdcd904841fc051b56b5cd21abe70d7385
Rails Training <mick.lewis84@gmail.com>
2015-12-07 10:25:11 +0000

    add article and category association

---
+++ test/integration/create_article_categories_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+
+class CreateArticleCategoriesTest < ActionDispatch::IntegrationTest
+    
+   test "get new article category form and create article category" do
+       
+   end
+    
+end
